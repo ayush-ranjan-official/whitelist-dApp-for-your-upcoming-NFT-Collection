7171468477cf2d9f56ff66f3dab5a404105536c0
Ayush <ayushranjanofficial27@gmail.com>
2023-06-02 22:37:21 +0530

    2nd commit - updated relative path

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from 'react';
-import { WHITELIST_CONTRACT_ADDRESS, abi } from "constants";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "./constants";
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
