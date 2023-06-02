0e2fe89b5fd39e2516a9e5484e557cb791020c99
Ayush <ayushranjanofficial27@gmail.com>
2023-06-02 15:45:52 +0530

    first commit

---
+++ hardhat-folder/contracts/Whitelist.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+
+    // It is not necessary to use the public modifier for a state variable in Solidity.
+    // Using the public modifier generates an automatic getter function for the state variable, allowing other contracts or external entities to read its value. 
+    // Max number of whitelisted addresses allowed
+    // uint8 as max value is 10
+    uint8 public maxWhitelistedAddresses;
+
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false my default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
+
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    uint8 public numAddressesWhitelisted;
+
+    // set maxWhitelistedAddresses in start
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+    */
+   function addAddressToWhitelist() public {
+    // check if the user has already been whitelisted
+    require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+    // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+    require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses can't be added, limit reached");
+    // Add the address which called the function to the whitelistedAddress array
+    whitelistedAddresses[msg.sender] = true;
+    // Increase the number of whitelisted addresses
+    numAddressesWhitelisted += 1;
+   }
+
+
+}
---
+++ my-app/app/constants/page.js
@@ -0,0 +1,68 @@
+export const WHITELIST_CONTRACT_ADDRESS = "0x8D7C2D3998b56e5fD398e7405A496E1a821F6974";
+
+
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxWhitelistedAddresses",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "addAddressToWhitelist",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "maxWhitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numAddressesWhitelisted",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
